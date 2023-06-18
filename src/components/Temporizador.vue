7c94043bf5f88c3872cc620c5f6a56dc770792ad
Rodrigo Gomes <cklrod@hotmail.com>
2023-06-18 14:24:50 -0300

    desabilitando e habilitando botões de play e stop

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="is-flex is-align-items-center is-justify-content-space-between">
                     <Cronometro :temporizador="temporizador"/>
-                    <button class="button" @click="iniciar()">
+                    <button class="button" @click="iniciar()" :disabled="cronometroRodando">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>play</span>
                     </button>
-                    <button class="button" @click="finalizar()">
+                    <button class="button" @click="finalizar()" :disabled="!cronometroRodando">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -28,18 +28,20 @@ export default defineComponent({
     data() {
         return {
             temporizador: 0,
-            cronometro: 0
+            cronometro: 0,
+            cronometroRodando: false
         }
     },
    
     methods: {
         iniciar() {
-            console.log('iniciar')
+            this.cronometroRodando = true
             this.cronometro = setInterval(() => {
                 this.temporizador++;
             }, 1000)
         },
         finalizar() {
+            this.cronometroRodando = false
             clearInterval(this.cronometro)
         },
     }
